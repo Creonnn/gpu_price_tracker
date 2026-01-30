09615969a86008e504f0f237952c762b32a6bdca
Linh Vuong <linhvuong@mac.home>
2026-01-29 22:24:01 -0500

    Updated test_extract.py get_5090_memexp_paths with new tests

--- test/test_extract.py
+++ test/test_extract.py
@@ -1,14 +1,21 @@
 import pytest
 from tools.extract import Extract
+from tools.scraper import get_5090_memexp_ids
 import pandas as pd
+import asyncio
 
+def test_get_5090_memexp_ids():
+    base_url = "https://www.memoryexpress.com"
+    path = "/Category/VideoCards"
+    filter_id = "FilterID=825f91ad-c322-6b30-5411-a027616f0c32"
+    paths = asyncio.run(get_5090_memexp_ids(base_url, path, filter_id))
+    # Return object is a list
+    assert isinstance(paths, list)
+    # Objects in the list are strings
+    assert all([isinstance(id, str) for id in paths])
 
 def test_extract():
-    links = ["https://www.canadacomputers.com/en/powered-by-nvidia/268435/msi-geforce-rtx-5090-32g-ventus-3x-oc-rtx-5090-32g-ventus-3x-oc.html"]
-    data = Extract(links)
-
-    # Test if df atttibute for Extract object is a pandas DataFrame
-    assert isinstance(data, pd.DataFrame)
+    pass
 
 
 if __name__ == "__main__":
