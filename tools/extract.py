8c95ae832ae97030778cb00a3660da773fa4caa4
Kun Zhao <kunzhaoz@gmail.com>
2026-01-29 22:35:55 -0500

    Updated extract.py to extract and clean name and price of item from webpage. Load data into df

--- tools/extract.py
+++ tools/extract.py
@@ -1,42 +1,60 @@
-import requests
-from bs4 import BeautifulSoup
-from tools.const.links import cc_links
 import pandas as pd
 import warnings
+import asyncio
+from tools.scraper import assemble_url, get_5090_memexp_paths
+from playwright.async_api import async_playwright
 warnings.filterwarnings("ignore")
 
 class Extract:
     
-    def __init__(self, links):
+    def __init__(self, base_url, paths):
         self.df = pd.DataFrame(columns=["name", "link", "price"])
-
-        for link in links:
-            response = requests.get(link)
-            soup = BeautifulSoup(response.text, "html.parser")
-            name = Extract._get_item_name(soup)
-            price = Extract._get_item_price(soup)
-            self.df.loc[len(self.df)] = [link, name, price]
+        self.base_url = base_url
+        self.paths = paths
 
 
-    def _get_item_name(self, soup):
-        pass
-
-    def _get_item_price(self, soup):
-        pass
-
-    def _get_availability(self, soup):
-        pass
-
-if __name__ == "__main__":  
-    import asyncio
-    from playwright.async_api import async_playwright
-
-    url = "https://www.memoryexpress.com/Products/MX00132651"
-    async def main():
+    async def extract_data(self):
         async with async_playwright() as p:
             browser = await p.chromium.launch()
             page = await browser.new_page()
-            await page.goto(url)
-            print(await page.title())
+            for path in self.paths:
+                url = assemble_url(base_url=self.base_url, path=path)
+                await page.goto(url)
+                name = self._clean_name(await page.title())
+                price = self._clean_price(await page.locator('[class="c-capr-pricing__totals"]')\
+                                .first\
+                                .inner_text())
+                new = {'name': name, 'link': url, 'price': price}
+                self.df.loc[len(self.df)] = new
             await browser.close()
-    asyncio.run(main())
+
+    def _clean_name(self, name):
+
+        name = name.split(",")[0]\
+                .replace("Dual HDMI", "")\
+                .replace("HDMI", "")\
+                .replace("w/", "")\
+                .replace("PCI-E", "")\
+                .replace("Triple DP", "")\
+                .replace("Graphics Card", "")\
+                .replace("32GB", "")\
+                .strip()
+    
+        return " ".join(name.split())
+
+    def _clean_price(self, price):
+        
+        price = price.replace("Only", "")\
+                    .replace("$", "")\
+                    .replace(",", "")
+        return float(price)
+
+
+if __name__ == "__main__":  
+    base_url = "https://www.memoryexpress.com"
+    catalog_path = "/Category/VideoCards"
+    filter_id = "FilterID=825f91ad-c322-6b30-5411-a027616f0c32"
+    paths = asyncio.run(get_5090_memexp_paths(base_url, catalog_path, filter_id))
+    data = Extract(base_url, paths)
+    asyncio.run(data.extract_data())
+    print(data.df)
--- tools/scraper.py
+++ tools/scraper.py
@@ -2,23 +2,25 @@ import asyncio
 from playwright.async_api import async_playwright
 
 # Todo: refactor so that it works for any bestbuy and newegg
-async def get_5090_memexp_links(base_url="https://www.memoryexpress.com", path=None, filter_id=None):
+async def get_5090_memexp_paths(base_url, path=None, filter_id=None):
         async with async_playwright() as p:
             browser = await p.chromium.launch()
-            assembled_url = assemble_url(base_url, path, filter_id) if path else base_url
+            assembled_url = assemble_url(base_url, path, filter_id)
             page = await browser.new_page()
             await page.goto(assembled_url)
 
-            # Get the links
-            links = page.locator('[data-role="product-list-container"]')\
+            # Get the paths to the item
+            # Todo: refactor this?
+            paths = page.locator('[data-role="product-list-container"]')\
                         .locator('[class="c-shca-icon-item__body-name"]')\
                         .locator('a')
-            links = [await links.nth(i).get_attribute("href") for i in range(await links.count())]
+            paths = [await paths.nth(i).get_attribute("href") for i in range(await paths.count())]
 
             await browser.close()
-            return links
+          
+            return paths
         
-def assemble_url(base_url, path, filter_id):
+def assemble_url(base_url, path=None, filter_id=None):
      """
      Assembles base_url with path and/or filter_id
      
@@ -43,11 +45,14 @@ def assemble_url(base_url, path, filter_id):
      if path and filter_id:
           return f"{base_url}{path}?{filter_id}"
      
-     return f"{base_url}{path}"
+     elif path:
+          return f"{base_url}{path}"
+     
+     return base_url
      
 
 if __name__ == "__main__":
     base_url = "https://www.memoryexpress.com"
     path = "/Category/VideoCards"
     filter_id = "FilterID=825f91ad-c322-6b30-5411-a027616f0c32"
-    print(asyncio.run(get_5090_memexp_links(base_url, path, filter_id)))
+    print(asyncio.run(get_5090_memexp_paths(base_url, path, filter_id)))
